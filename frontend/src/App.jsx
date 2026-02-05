e59a02edec4658afb5cf1196baf5a7b6ac8b375e
robertmcabee <robertthemcabee@gmail.com>
2026-02-05 13:18:56 -0700

    install leaflet and react-leaflet #3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,17 @@
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Slider from '@mui/material/Slider';
-
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import LeafletMap from './LeafletMap'
 
 export default function App() {
+
   return (
     <Container maxWidth="md">
-      <div className="my-8">
-        <Typography variant="h4" component="h1" color='primary' sx={{ mb: 2 }}>
-          Hello World
-        </Typography>
-        <Slider
-          className="my-4"
-          defaultValue={30}
-          classes={{ active: 'shadow-none' }}
-          slotProps={{ thumb: { className: 'hover:shadow-none' } }}
-        />
-        <Copyright />
-      </div>
+        <p className='font-bold mt-8'>Leaflet Map</p>
+        <Container className='bg-gray-200 min-h-100 mt-8' >
+          <LeafletMap/>
+        </Container>
+        
     </Container>
   );
 }
