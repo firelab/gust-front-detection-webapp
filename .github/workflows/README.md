059d2888d9cea9e60f7ac520ede18cebe47c1de5
William Elder <williamxelder@gmail.com>
2026-03-03 07:41:16 -0700

    Forgot to add all of the changes from the last edit on the code base. Fixed the runner and implemented redis

---
+++ .github/workflows/README.md
@@ -0,0 +1,5 @@
+# This folder is for the setting up of the linter.
+
+    I, William, do not have the linter set up as of now. If you find this folder and directory path, don't worry about it. Over the next couple weeks, I will keep reading the documentation so that we can get this set up properly
+
+
--- backend/apis/job_status.py
+++ backend/apis/job_status.py
@@ -6,7 +6,8 @@ from flask import jsonify
 from src.nfgda_service.nfgda_service import NfgdaService
 
 
-def get_status(job_id: str, nfgda_service: NfgdaService):
+# Add redis client to the arguments so that it can see the container
+def get_status(job_id: str, redis_client, nfgda_service: NfgdaService):
     """
     Response shape:
     {
@@ -15,4 +16,14 @@ def get_status(job_id: str, nfgda_service: NfgdaService):
         "error": ""
     }
     """
-    pass
+
+    job = redis_client.hgetall(job_id)  # reads the hash from Redis
+
+    if not job:
+        # job doesn't exist — return a 404
+        return jsonify({"error": "Job not found"}), 404
+
+    # job exists — return id and status as JSON
+    return jsonify(
+        {"id": job_id, "status": job.get("status", ""), "error": job.get("error", "")}
+    ), 200
