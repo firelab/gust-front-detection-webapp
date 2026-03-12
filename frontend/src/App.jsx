5ce75e8e0c0c45e3bf04caa8ca2b5c3eed1905d0
robertmcabee <robertthemcabee@gmail.com>
2026-03-12 12:24:39 -0600

    add currentMode toggle #34

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,16 +10,17 @@ import { LocalizationProvider, DateTimePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
-import { Slider, Button, Select, MenuItem, FormControl, FormLabel, FormHelperText, InputLabel } from '@mui/material';
+import { Slider, Button, Select, MenuItem, FormControl, InputLabel, Checkbox } from '@mui/material';
 
 export default function App() {
 
   const [stations, setStations] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentMode, setCurrentMode] = useState(true);
   const [selectedStation, setSelectedStation] = useState("");
   const [selectedDateTime, setSelectedDateTime] = useState(dayjs().tz(dayjs.tz.guess()).startOf('hour'));
   const [timezone, setTimezone] = useState(dayjs.tz.guess());
-
+  
   // fetch radar stations from backend API
   useEffect(() => {
     async function loadStations() {
@@ -48,7 +49,7 @@ export default function App() {
 
   return (
     <Container maxWidth="md">
-        <div className='mt-20 gap-4 flex'>
+        <div className='mt-20 gap-4 flex items-end'>
           {/* Station Selector */}
           <div className='flex-1'>
             <RadarStationDropdown
@@ -57,37 +58,48 @@ export default function App() {
               setSelectedStation={setSelectedStation}
             />
           </div>
-          {/* Timezone Selector */}
-          <FormControl>
-            <InputLabel>Timezone</InputLabel>
-            <Select
-              value={timezone}
-              className='min-w-24'
-              label='Timezone'
-              onChange={handleTimezoneChange}
-            >
-              <MenuItem value="UTC">UTC</MenuItem>
-              <MenuItem value="America/Los_Angeles">Pacific</MenuItem>
-              <MenuItem value="America/Denver">Mountain</MenuItem>
-              <MenuItem value="America/Chicago">Central</MenuItem>
-              <MenuItem value="America/New_York">Eastern</MenuItem>
-              <MenuItem value="America/Anchorage">Alaska</MenuItem>
-            </Select>
-          </FormControl>
-          {/* Date Time Selector */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateTimePicker
-              views={['year', 'month', 'day', 'hours']}
-              label="Radar Data Start Time"
-              value={selectedDateTime}
-              onChange={(newValue) => setSelectedDateTime(dayjs(newValue).tz(timezone).startOf('hour'))}
-              defaultValue={dayjs('2026-03-11T15:00')}
-              className='flex-1'
-            />
-          </LocalizationProvider>
+          <div className="flex flex-col">
+            <div className="flex items-center">
+              <Checkbox value={currentMode} onChange={()=>{setCurrentMode(!currentMode)}}>
+              </Checkbox>
+              <p>Use Current Data</p>
+            </div>
+            <div>
+            {/* Timezone Selector */}
+              <FormControl>
+                <InputLabel>Timezone</InputLabel>
+                <Select
+                  disabled={!currentMode}
+                  value={timezone}
+                  className='min-w-24 mr-1'
+                  label='Timezone'
+                  onChange={handleTimezoneChange}
+                >
+                  <MenuItem value="UTC">UTC</MenuItem>
+                  <MenuItem value="America/Los_Angeles">Pacific</MenuItem>
+                  <MenuItem value="America/Denver">Mountain</MenuItem>
+                  <MenuItem value="America/Chicago">Central</MenuItem>
+                  <MenuItem value="America/New_York">Eastern</MenuItem>
+                  <MenuItem value="America/Anchorage">Alaska</MenuItem>
+                </Select>
+              </FormControl>
+              {/* Date Time Selector */}
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker
+                  disabled={!currentMode}
+                  views={['year', 'month', 'day', 'hours']}
+                  label="Radar Data Start Time"
+                  value={selectedDateTime}
+                  onChange={(newValue) => setSelectedDateTime(dayjs(newValue).tz(timezone).startOf('hour'))}
+                  defaultValue={dayjs('2026-03-11T15:00')}
+                  className='flex-1'
+                />
+              </LocalizationProvider>
+            </div>
+          </div>
           {/* Fetch Button */}
           <Button
-            className="w-[20%]" 
+            className="w-[20%] h-14" 
             onClick={()=>{console.log(selectedStation.properties.station_id + selectedDateTime.utc().format())}}
             variant="contained">
               Get Radar Data
