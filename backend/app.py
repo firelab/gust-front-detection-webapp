68825d0842bb4d762867112d5a88e3cfffd0e86c
Eoghan Perkins <perkinseoghan@gmail.com>
2026-03-12 12:21:20 -0600

    minor docker compose fixes

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 from apis.stations import list_stations_api
 from apis.run_request import send_job_to_redis_queue
 from apis.status import get_job_status
+from apis.retrieve_frames import get_frames
 
 app = Flask(__name__)
 
@@ -26,7 +27,7 @@ def stations_endpoint():
 
 # Algorithm Runner API
 @app.route("/APIs/run", methods=["POST"])
-def start_run():
+def run_endpoint():
     """Takes station and time frame args, kicks off an NFGDA processing job, and returns the new job ID and status code."""
     if not request.json:
         return jsonify({"error": "Missing request body"}), 400
@@ -35,10 +36,13 @@ def start_run():
     
 
 # Frame Data API
-@app.route("/APIs/frames/<job_id>", methods=["GET"])
-def get_frames(job_id: str):
+@app.route("/APIs/frames", methods=["GET"])
+def frames_endpoint():
     """Takes job ID, returns list of frames."""
-    pass
+    job_id = request.args.get("job_id")
+    if not job_id:
+        return jsonify({"error": "Missing job ID"}), 400
+    return get_frames(redis_client, job_id)
 
 
 # Job Status API
