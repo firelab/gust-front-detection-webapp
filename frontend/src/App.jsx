9c7ed84c18be8c0e71d163d0b2f04a96be563f7a
robertmcabee <robertthemcabee@gmail.com>
2026-03-31 16:08:40 -0600

    lint & format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,32 @@
-import Container from '@mui/material/Container';
-import 'leaflet/dist/leaflet.css';
-import LeafletMap from './components/LeafletMap'
-import RadarStationDropdown from "./components/RadarStationDropdown";
-import { useState, useEffect, useRef } from 'react';
-import dayjs from './utils/dayjsConfig';
-
+import Container from "@mui/material/Container";
+import "leaflet/dist/leaflet.css";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Slider,
+} from "@mui/material";
 // MUI
-import { LocalizationProvider, DateTimePicker } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import PauseIcon from '@mui/icons-material/Pause';
-import { Slider, Button, Select, MenuItem, FormControl, InputLabel, Checkbox } from '@mui/material';
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useEffect, useRef, useState } from "react";
+import LeafletMap from "./components/LeafletMap";
+import RadarStationDropdown from "./components/RadarStationDropdown";
+import dayjs from "./utils/dayjsConfig";
 
 export default function App() {
-
   // User Selection State
   const [stations, setStations] = useState([]);
   const [currentMode, setCurrentMode] = useState(true);
   const [selectedStation, setSelectedStation] = useState("");
-  const [selectedDateTime, setSelectedDateTime] = useState(dayjs().tz(dayjs.tz.guess()));
+  const [selectedDateTime, setSelectedDateTime] = useState(
+    dayjs().tz(dayjs.tz.guess()),
+  );
   const [timezone, setTimezone] = useState(dayjs.tz.guess());
 
   // API State
@@ -32,7 +40,7 @@ export default function App() {
   const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const playbackRef = useRef(null);
-  
+
   // --------------------------------------- HANDLERS ----------------------------------------
 
   // requests a job from /backend/apis/run_request.py and recieves a job_id and response code
@@ -45,18 +53,29 @@ export default function App() {
       }
       setErrorMessage("");
       const requestBody = {
-          stationId: selectedStation.properties.station_id
+        stationId: selectedStation.properties.station_id,
       };
-      if (!currentMode){
+      if (!currentMode) {
         console.log("using historical data");
         // if not fetching current data the end time requested is t+30 minutes
-        requestBody.startUtc = selectedDateTime.utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
-        requestBody.endUtc = selectedDateTime.add(30, 'minute').utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
+        requestBody.startUtc = selectedDateTime
+          .utc()
+          .format("YYYY-MM-DDTHH:mm:ss[Z]");
+        requestBody.endUtc = selectedDateTime
+          .add(30, "minute")
+          .utc()
+          .format("YYYY-MM-DDTHH:mm:ss[Z]");
       } else {
         // currently using the same timebox as the default in run_request.py when no timebox is provided
         console.log("using current data");
-        requestBody.startUtc = dayjs().subtract(45, 'minute').utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
-        requestBody.endUtc = dayjs().subtract(25, 'minute').utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
+        requestBody.startUtc = dayjs()
+          .subtract(45, "minute")
+          .utc()
+          .format("YYYY-MM-DDTHH:mm:ss[Z]");
+        requestBody.endUtc = dayjs()
+          .subtract(25, "minute")
+          .utc()
+          .format("YYYY-MM-DDTHH:mm:ss[Z]");
       }
 
       // ---- reset state ----
@@ -67,23 +86,26 @@ export default function App() {
 
       // ---- make request ----
       const response = await fetch("/apis/run", {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
-        body: JSON.stringify(requestBody)
+        body: JSON.stringify(requestBody),
       });
 
       // ---- Handle Errors ----
       if (!response.ok) {
-        const errorData = await response.json().catch(() => ({})); 
-        const errorMsg = errorData.error || errorData.message || `Error ${response.status}: ${response.statusText}`;
+        const errorData = await response.json().catch(() => ({}));
+        const errorMsg =
+          errorData.error ||
+          errorData.message ||
+          `Error ${response.status}: ${response.statusText}`;
         setErrorMessage(errorMsg);
         setJobStatus("FAILED");
         throw new Error(errorMsg); // This sends the message to the catch block
       }
-      
+
       const data = await response.json();
       setjobId(data.job_id);
     } catch (err) {
@@ -96,28 +118,28 @@ export default function App() {
   };
 
   // fetch frames once the job is completed and the jobId and numFrames are set
-useEffect(() => {
-  async function fetchFrames() {
-    if (jobStatus !== "COMPLETED" || !jobId || numFrames <= 0) return;
-    console.log(`attempting to fetch ${numFrames} frames for job ${jobId}`);
-    try {
-      const promises = Array.from({ length: numFrames }, (_, i) =>
-        fetch(`/apis/jobs/${jobId}/frames/${i}`)
-          .then(res => {
-            if (!res.ok) throw new Error(`Failed frame ${i}`);
-            return res.blob();
-          })
-          .then(blob => URL.createObjectURL(blob))
-      );
-      const urls = await Promise.all(promises);
-      setFrames(urls);
-      console.log("Frames fetched successfully: ", urls);
-    } catch (err) {
-      console.error("Error fetching frames:", err);
+  useEffect(() => {
+    async function fetchFrames() {
+      if (jobStatus !== "COMPLETED" || !jobId || numFrames <= 0) return;
+      console.log(`attempting to fetch ${numFrames} frames for job ${jobId}`);
+      try {
+        const promises = Array.from({ length: numFrames }, (_, i) =>
+          fetch(`/apis/jobs/${jobId}/frames/${i}`)
+            .then((res) => {
+              if (!res.ok) throw new Error(`Failed frame ${i}`);
+              return res.blob();
+            })
+            .then((blob) => URL.createObjectURL(blob)),
+        );
+        const urls = await Promise.all(promises);
+        setFrames(urls);
+        console.log("Frames fetched successfully: ", urls);
+      } catch (err) {
+        console.error("Error fetching frames:", err);
+      }
     }
-  }
-  fetchFrames();
-}, [jobStatus, jobId, numFrames]);
+    fetchFrames();
+  }, [jobStatus, jobId, numFrames]);
 
   // fetch radar stations from backend at /apis/stations
   useEffect(() => {
@@ -144,31 +166,30 @@ useEffect(() => {
         const data = await response.json();
         console.log(data);
         setJobStatus(data.status);
-        if (data.error){
+        if (data.error) {
           setErrorMessage(data.error_message);
           console.log("here");
-          
         } else {
           setErrorMessage("");
         }
-        if (data.num_frames){
+        if (data.num_frames) {
           setNumFrames(data.num_frames);
         } else {
           setNumFrames(0);
         }
-        } catch (err) {
-          console.error(err);
-        }
+      } catch (err) {
+        console.error(err);
+      }
     }, 5000);
     return () => clearInterval(intervalId);
   }, [jobId, jobStatus]);
 
   // timezone change handler
   function handleTimezoneChange(event) {
-    const newTZ = event.target.value
-    setTimezone(newTZ)
+    const newTZ = event.target.value;
+    setTimezone(newTZ);
     if (selectedDateTime) {
-      setSelectedDateTime(selectedDateTime.tz(newTZ))
+      setSelectedDateTime(selectedDateTime.tz(newTZ));
     }
   }
 
@@ -186,7 +207,7 @@ useEffect(() => {
   }, [isPlaying, frames.length]);
 
   // Handle Slider Change
-  const handleSliderChange = (event, newValue) => {
+  const handleSliderChange = (newValue) => {
     setIsPlaying(false);
     setCurrentFrameIndex(newValue);
   };
@@ -195,94 +216,109 @@ useEffect(() => {
 
   return (
     <Container maxWidth="md">
-        <div className='mt-20 gap-4 flex flex-col md:flex-row md:items-end'>
-          {/* Station Selector */}
-          <div className='flex-1'>
-            <RadarStationDropdown
-              stations={stations}
-              selectedStation={selectedStation}
-              setSelectedStation={setSelectedStation}
-            />
-          </div>
-          <div className="flex flex-col">
-            <div className="flex items-center">
-              <Checkbox checked={currentMode} onChange={()=>{setCurrentMode(!currentMode)}}>
-              </Checkbox>
-              <p>Use Current Data</p>
-            </div>
-            <div>
-            {/* Timezone Selector */}
-              <FormControl>
-                <InputLabel>Timezone</InputLabel>
-                <Select
-                  disabled={currentMode}
-                  value={timezone}
-                  className='min-w-24 mr-1'
-                  label='Timezone'
-                  onChange={handleTimezoneChange}
-                >
-                  <MenuItem value="UTC">UTC</MenuItem>
-                  <MenuItem value="America/Anchorage">Alaska</MenuItem>
-                  <MenuItem value="America/Los_Angeles">Pacific</MenuItem>
-                  <MenuItem value="America/Denver">Mountain</MenuItem>
-                  <MenuItem value="America/Chicago">Central</MenuItem>
-                  <MenuItem value="America/New_York">Eastern</MenuItem>
-                </Select>
-              </FormControl>
-              {/* Date Time Selector */}
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateTimePicker
-                  disabled={currentMode}
-                  views={['year', 'month', 'day', 'hours', 'minutes']}
-                  ampm={false}
-                  label="Radar Data Start Time"
-                  value={selectedDateTime}
-                  onChange={(newValue) => setSelectedDateTime(dayjs(newValue).tz(timezone))}
-                  defaultValue={dayjs('2026-03-11T15:00')}
-                  className='flex-1'
-                />
-              </LocalizationProvider>
-            </div>
-          </div>
-          {/* Fetch Button */}
-          <Button
-            className="md:w-[20%] h-14" 
-            onClick={fetchRadarData}
-            variant="contained">
-              Get Radar Data
-          </Button>
-        </div>
-        {jobStatus === "PROCESSING" && <p>The radar data is being processed. This usually takes a couple minutes.</p>}
-        {jobStatus === "REQUESTED" && <p>The radar data has been requested. Please wait.</p>}
-        {errorMessage&& <p className='font-bold'>{errorMessage}</p>}
-
-        <div className="flex w-full mt-20 mb-2 items-center px-4">
-          <button 
-            onClick={() => setIsPlaying(!isPlaying)}
-            className='mr-4 cursor-pointer text-white rounded-full bg-[#1976d2] hover:bg-[#1565c0] shadow hover:shadow-lg transition-all flex p-3 h-max'>
-            {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
-          </button>
-          <Slider
-            value={currentFrameIndex}
-            min={0}
-            max={frames.length - 1}
-            step={1}
-            onChange={handleSliderChange}
-            valueLabelDisplay="auto"
-            marks={frames.map((_, i) => ({ value: i }))}
-          />
-        </div>
-
-        <Container className='bg-gray-50 min-h-100' >
-          <LeafletMap
+      <div className="mt-20 gap-4 flex flex-col md:flex-row md:items-end">
+        {/* Station Selector */}
+        <div className="flex-1">
+          <RadarStationDropdown
             stations={stations}
             selectedStation={selectedStation}
             setSelectedStation={setSelectedStation}
-            frames={frames}
-            currentFrameIndex={currentFrameIndex}
           />
-        </Container>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <Checkbox
+              checked={currentMode}
+              onChange={() => {
+                setCurrentMode(!currentMode);
+              }}
+            ></Checkbox>
+            <p>Use Current Data</p>
+          </div>
+          <div>
+            {/* Timezone Selector */}
+            <FormControl>
+              <InputLabel>Timezone</InputLabel>
+              <Select
+                disabled={currentMode}
+                value={timezone}
+                className="min-w-24 mr-1"
+                label="Timezone"
+                onChange={handleTimezoneChange}
+              >
+                <MenuItem value="UTC">UTC</MenuItem>
+                <MenuItem value="America/Anchorage">Alaska</MenuItem>
+                <MenuItem value="America/Los_Angeles">Pacific</MenuItem>
+                <MenuItem value="America/Denver">Mountain</MenuItem>
+                <MenuItem value="America/Chicago">Central</MenuItem>
+                <MenuItem value="America/New_York">Eastern</MenuItem>
+              </Select>
+            </FormControl>
+            {/* Date Time Selector */}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                disabled={currentMode}
+                views={["year", "month", "day", "hours", "minutes"]}
+                ampm={false}
+                label="Radar Data Start Time"
+                value={selectedDateTime}
+                onChange={(newValue) =>
+                  setSelectedDateTime(dayjs(newValue).tz(timezone))
+                }
+                defaultValue={dayjs("2026-03-11T15:00")}
+                className="flex-1"
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+        {/* Fetch Button */}
+        <Button
+          className="md:w-[20%] h-14"
+          onClick={fetchRadarData}
+          variant="contained"
+        >
+          Get Radar Data
+        </Button>
+      </div>
+      {jobStatus === "PROCESSING" && (
+        <p>
+          The radar data is being processed. This usually takes a couple
+          minutes.
+        </p>
+      )}
+      {jobStatus === "REQUESTED" && (
+        <p>The radar data has been requested. Please wait.</p>
+      )}
+      {errorMessage && <p className="font-bold">{errorMessage}</p>}
 
+      <div className="flex w-full mt-20 mb-2 items-center px-4">
+        <button
+          type="button"
+          onClick={() => setIsPlaying(!isPlaying)}
+          className="mr-4 cursor-pointer text-white rounded-full bg-[#1976d2] hover:bg-[#1565c0] shadow hover:shadow-lg transition-all flex p-3 h-max"
+        >
+          {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
+        </button>
+        <Slider
+          value={currentFrameIndex}
+          min={0}
+          max={frames.length - 1}
+          step={1}
+          onChange={handleSliderChange}
+          valueLabelDisplay="auto"
+          marks={frames.map((_, i) => ({ value: i }))}
+        />
+      </div>
+
+      <Container className="bg-gray-50 min-h-100">
+        <LeafletMap
+          stations={stations}
+          selectedStation={selectedStation}
+          setSelectedStation={setSelectedStation}
+          frames={frames}
+          currentFrameIndex={currentFrameIndex}
+        />
+      </Container>
     </Container>
   );
 }
--- frontend/src/components/GeoTiffAnimation.jsx
+++ frontend/src/components/GeoTiffAnimation.jsx
@@ -1,7 +1,7 @@
+import parseGeoraster from "georaster";
+import L from "leaflet";
 import { useEffect, useState } from "react";
 import { ImageOverlay } from "react-leaflet";
-import L from "leaflet";
-import parseGeoraster from "georaster";
 
 export default function GeotiffAnimation({ frames, currentIndex }) {
   const [frameData, setFrameData] = useState([]);
@@ -9,51 +9,59 @@ export default function GeotiffAnimation({ frames, currentIndex }) {
 
   useEffect(() => {
     async function processFrames() {
-      const processed = await Promise.all(frames.map(async (url) => {
-        const res = await fetch(url);
-        const buf = await res.arrayBuffer();
-        const geo = await parseGeoraster(buf);
+      const processed = await Promise.all(
+        frames.map(async (url) => {
+          const res = await fetch(url);
+          const buf = await res.arrayBuffer();
+          const geo = await parseGeoraster(buf);
 
-        const canvas = document.createElement('canvas');
-        const w = canvas.width = geo.width;
-        const h = canvas.height = geo.height;
-        const ctx = canvas.getContext('2d');
-        const imgData = ctx.createImageData(w, h);
-        const data = imgData.data;
+          const canvas = document.createElement("canvas");
+          canvas.width = geo.width;
+          const w = canvas.width;
+          canvas.height = geo.height;
+          const h = canvas.height;
+          const ctx = canvas.getContext("2d");
+          const imgData = ctx.createImageData(w, h);
+          const data = imgData.data;
 
-        // Cache band references
-        const bands = geo.values;
-        const numBands = bands.length;
+          // Cache band references
+          const bands = geo.values;
+          const numBands = bands.length;
 
-        for (let i = 0; i < w * h; i++) {
-          const x = i % w;
-          const y = (i / w) | 0;
-          const i4 = i << 2;
+          for (let i = 0; i < w * h; i++) {
+            const x = i % w;
+            const y = (i / w) | 0;
+            const i4 = i << 2;
 
-          if (numBands >= 3) {
-            data[i4]     = bands[0][y][x]; // R
-            data[i4 + 1] = bands[1][y][x]; // G
-            data[i4 + 2] = bands[2][y][x]; // B
-            data[i4 + 3] = numBands === 4 ? bands[3][y][x] : 255;
-          } else {
-            const v = bands[0][y][x];
-            data[i4] = data[i4 + 1] = data[i4 + 2] = v;
-            data[i4 + 3] = 255;
+            if (numBands >= 3) {
+              data[i4] = bands[0][y][x]; // R
+              data[i4 + 1] = bands[1][y][x]; // G
+              data[i4 + 2] = bands[2][y][x]; // B
+              data[i4 + 3] = numBands === 4 ? bands[3][y][x] : 255;
+            } else {
+              const v = bands[0][y][x];
+              data[i4] = data[i4 + 1] = data[i4 + 2] = v;
+              data[i4 + 3] = 255;
+            }
           }
-        }
-        ctx.putImageData(imgData, 0, 0);
+          ctx.putImageData(imgData, 0, 0);
 
-        const southWest = L.Projection.SphericalMercator.unproject(L.point(geo.xmin, geo.ymin));
-        const northEast = L.Projection.SphericalMercator.unproject(L.point(geo.xmax, geo.ymax));
+          const southWest = L.Projection.SphericalMercator.unproject(
+            L.point(geo.xmin, geo.ymin),
+          );
+          const northEast = L.Projection.SphericalMercator.unproject(
+            L.point(geo.xmax, geo.ymax),
+          );
 
-        return {
-          url: canvas.toDataURL(),
-          bounds: [
-            [southWest.lat, southWest.lng],
-            [northEast.lat, northEast.lng]
-          ]
-        };
-      }));
+          return {
+            url: canvas.toDataURL(),
+            bounds: [
+              [southWest.lat, southWest.lng],
+              [northEast.lat, northEast.lng],
+            ],
+          };
+        }),
+      );
 
       setFrameData(processed);
       setLoading(false);
